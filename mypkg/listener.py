45fce71b844a19f59f577e86bcb0bb739ff50f1c
Fukuda Taiki <s21C1101AP@s.chibakoudai.jp>
2022-12-27 22:53:27 +0900

    restore

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -2,50 +2,50 @@
 # SPDX-FileCopyrightText: 2022 Taiki Fukuda s21C1101AP@s.chibakoudai.jp
 # SPDX-License-Identifier: BSD-3-Clause
 
-#import rclpy
-#from rclpy.node import Node
-#from std_msgs.msg import Int16
-
-#def cb(msg):
-#    global node
-#    node.get_logger().info("Listen: %d" % msg.data)
-
-#rclpy.init()
-#node = Node("listener")
-#pub = node.create_subscription(Int16, "countup", cb, 10)
-#rclpy.spin(node)
-
 import rclpy
 from rclpy.node import Node
-from person_msgs.srv import Query
+from std_msgs.msg import Int16
 
-def main():
-    n = 0
-    rclpy.init()
-    node = Node("listener")
-    client = node.create_client(Query, 'query') 
-    while not client.wait_for_service(timeout_sec=1.0):
-        n += 1
-        node.get_logger().info("NOW_LOWDING:%d" % (n))
+def cb(msg):
+    global node
+    node.get_logger().info("Listen: %d" % msg.data)
+
+rclpy.init()
+node = Node("listener")
+pub = node.create_subscription(Int16, "countup", cb, 10)
+rclpy.spin(node)
+
+#import rclpy
+#from rclpy.node import Node
+#from person_msgs.srv import Query
+
+#def main():
+#    n = 0
+#    rclpy.init()
+#    node = Node("listener")
+#    client = node.create_client(Query, 'query') 
+#    while not client.wait_for_service(timeout_sec=1.0):
+#        n += 1
+#        node.get_logger().info("NOW_LOWDING:%d" % (n))
          
-    req = Query.Request()
-    req.you = "gu-"
-    future = client.call_async(req) 
+#    req = Query.Request()
+#    req.you = "gu-"
+#    future = client.call_async(req) 
 
-    while rclpy.ok():
-        rclpy.spin_once(node) 
-        if future.done():
-            try:
-                response = future.result()
-            except:
-                node.get_logger().info('error')
-            else:
-                node.get_logger().info("cup: {}".format(response.cpu))
+#    while rclpy.ok():
+#        rclpy.spin_once(node) 
+#        if future.done():
+#            try:
+#                response = future.result()
+#            except:
+#                node.get_logger().info('error')
+#            else:
+#                node.get_logger().info("cup: {}".format(response.cpu))
             
-            break
+#            break
     
-    node.destroy_node() 
-    rclpy.shutdown() 
+#    node.destroy_node() 
+#    rclpy.shutdown() 
 
-if __name__ == '__main__': 
-    main()
+#if __name__ == '__main__': 
+#    main()
--- mypkg/talker.py
+++ mypkg/talker.py
@@ -3,45 +3,45 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 import rclpy
-import random
+#import random
 from rclpy.node import Node
-#from std_msgs.msg import Int16
-from person_msgs.srv import Query
+from std_msgs.msg import Int16
+#from person_msgs.srv import Query
 
-y = ["pa-", "gu-" ,"choki"]
-result = random.choice(y)
-input("jyanken:")
+#y = ["pa-", "gu-" ,"choki"]
+#result = random.choice(y)
+#input("jyanken:")
 
 class Talker():
-#    def __init__(self, node):
-#        self.pub = node.create_publisher(Int16, "countup", 10)
-#        self.n = 0
-#        node.create_timer(0.5, self.cb)
+    def __init__(self, node):
+        self.pub = node.create_publisher(Int16, "countup", 10)
+        self.n = 0
+        node.create_timer(0.5, self.cb)
 
-#    def cb(self):
-#        msg = Int16()
-#        msg.data = self.n
-#        self.pub.publish(msg)
-#        self.n += 1
-    def cb(resquest, response):
-        result = random.choice(y)
-        if resquest.you == "gu-":
-            response.cpu = result
-        elif resquest.you == "choki":
-             response.cpu = result
-        elif resquest.you == "pa-":
-            response.cpu = result
-        else:
-             response.cpu = "error"
+    def cb(self):
+        msg = Int16()
+        msg.data = self.n
+        self.pub.publish(msg)
+        self.n += 1
+#    def cb(resquest, response):
+#        result = random.choice(y)
+#        if resquest.you == "gu-":
+#            response.cpu = result
+#        elif resquest.you == "choki":
+#             response.cpu = result
+#        elif resquest.you == "pa-":
+#            response.cpu = result
+#        else:
+#             response.cpu = "error"
 
-        return response
+#        return response
 
-srv = node.create_service(Query, "query", cb)
+
 
 def main():
     rclpy.init()
     node = Node("talker")
-#    talker = Talker(node)
+    talker = Talker(node)
 #    srv = node.create_service(Query, "query", cb)
     rclpy.spin(node)
 
